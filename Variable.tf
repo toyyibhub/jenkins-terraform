ba62131b9bbae924f66b3309e88fc196b8657ee0
toyyibhub <87342247+toyyibhub@users.noreply.github.com>
2021-08-02 22:31:27 -0500

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -1,1 +1,9 @@
-
+variable "access_key" {
+  default = "AKIAUW5TETSYUBWHA34A"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1"
+  }
